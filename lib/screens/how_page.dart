f076dbb0f10e8e23c2c92cf3e71381f89c432efe
atulya-kairati <atulyakairati@gmail.com>
2022-03-10 19:35:40 +0530

    Added some helpful comments

--- lib/screens/how_page.dart
+++ lib/screens/how_page.dart
@@ -5,7 +5,7 @@ class HowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('How to Fuck Bitches?'),
+      child: Text('Under construction LOL?'),
     );
   }
 }
